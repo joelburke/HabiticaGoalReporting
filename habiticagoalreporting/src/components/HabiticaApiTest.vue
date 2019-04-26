9861a66a6a9429f0eb36a136fb094df43370b08f
Joel Burke <jburke@bizstream.com>
2019-04-26 15:26:46 -0400

    Getting first call completedTodos returning json data for my user

--- habiticagoalreporting/src/components/HabiticaApiTest.vue
+++ habiticagoalreporting/src/components/HabiticaApiTest.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <b-form @submit="onSubmit">
+    <!-- <b-form @submit="onSubmit"> -->
+    <b-form>
         <b-form-group
             id="habiticaUsername-Group"
             label="Habitica Api Username Guid"
@@ -26,30 +27,53 @@
                 placeholder="enter password"></b-form-input>
         </b-form-group>
 
-        <b-button type="submit" variant="primary">Submit</b-button>
-        
-        <b-card class="mt-3" header="Form Data Result">
-            <pre class="m-0">{{ form }}</pre>
-        </b-card>
+        <b-button variant="primary" @click="onSubmit">Get User Data</b-button>        
     </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+        <pre class="m-0">{{ result }}</pre>
+    </b-card>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+
+const habiticaApiUrl='https://habitica.com/api/v3/tasks/user?type=completedTodos';
+
 export default {
   name: 'HabiticaApiTest',
   data: function () {
       return {
         msg: "Test",
         form: {
-            username: '',
-            password: ''
+            username: 'dd0cd644-3636-4249-bb8b-78d53ebecb42',
+            password: '8c8b9e6b-8505-48a4-a1eb-3b64cd01f96a'
+        },
+        result: '',
+        config: {
+            headers: { 
+                'Content-Type': 'application/json',
+                'x-api-user': 'dd0cd644-3636-4249-bb8b-78d53ebecb42',
+                'x-api-key': '8c8b9e6b-8505-48a4-a1eb-3b64cd01f96a'
+            },
         }
       }
   },
   methods: {
       onSubmit: function () {
-          console.log('hey you');
+          axios.get(
+              habiticaApiUrl,
+              this.config
+          ).then(
+              response => {
+                  this.result = response.data
+              }
+          ).catch(
+              error => {
+                  console.log('ERROR', error)
+              }
+          )
       }
   }
 }
